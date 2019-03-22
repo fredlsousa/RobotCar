b996f5ece2ada8c411f17fbf892c3e1bf08c8575
frederico <frederico.sousa@aluno.ufop.edu.br>
2019-03-22 15:29:30 -0300

    yyy

--- MotorDClass.py
+++ MotorDClass.py
@@ -10,20 +10,20 @@ class MotorDC():
         GPIO.setup(self.pinA, GPIO.OUT)
         GPIO.setup(self.pinB, GPIO.OUT)
         GPIO.setup(self.pinPWM, GPIO.OUT)
-        self.pwm = GPIO.PWM(self.pinPWM, 10)
+        self.pwm = GPIO.PWM(self.pinPWM, 30)
         self.pwm.start(0)
 
     def moveForward(self):
         GPIO.output(self.pinA, True)
         GPIO.output(self.pinB, False)
-        self.pwm.ChangeDutyCycle(10)
+        self.pwm.ChangeDutyCycle(30)
         GPIO.output(self.pinPWM, True)
         sleep(0.030)
 
     def moveBackwards(self):
         GPIO.output(self.pinA, False)
         GPIO.output(self.pinB, True)
-        self.pwm.ChangeDutyCycle(10)
+        self.pwm.ChangeDutyCycle(30)
         GPIO.output(self.pinPWM, True)
         sleep(0.030)
 
