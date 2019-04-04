e811c67e249447bd95227395f96aabca871d19d9
MacFred <frederico.sousa@aluno.ufop.edu.br>
2019-04-04 17:13:01 -0300

    yolo

--- testDisplacement.py
+++ testDisplacement.py
@@ -8,19 +8,32 @@ GPIO.setmode(GPIO.BOARD)
 
 mpuSensor = mpu6050(0x68)
 
+def getVelocity_Y():
+    time1 = []
+    accelerationY = []
+    time1.append(time.time())
+    for i in range(0, 4):
+        accelData = mpuSensor.get_accel_data()
+        accelerationY.append(accelData['y'])
+    time1.append(time.time())
+    deltaT = calc_deltat(time1)
+    velocity_Y = integration_trapeze(0, 3, accelerationY, deltaT)
+    return velocity_Y
+
 if __name__ == '__main__':
     try:
         while True:
             time1 = []
-            accelerationY = []
+            vListY = []
             time1.append(time.time())
-            for i in range(1, 4):
-                accelData = mpuSensor.get_accel_data()
-                accelerationY.append(accelData['y'])
+            for i in range(0, 4):
+                velocity_Y = getVelocity_Y()
+                vListY.append(velocity_Y)
             time1.append(time.time())
             deltaT = calc_deltat(time1)
-            velocity_Y = integration_trapeze(1, 4, accelerationY, deltaT)
-            print "Velocity = ", velocity_Y
+            displacementY = integration_trapeze(0, 3, vListY, deltaT)
+            print("Velocity\t Displacement\t Time\n")
+            print("%f\t %f\t %f\n") % vListY[3], displacementY, deltaT
             time.sleep(0.5)
     except KeyboardInterrupt:
         print ("Stopping")
