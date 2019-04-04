291e750cd47999d5a774c1b5b5cd14891c633b4e
MacFred <frederico.sousa@aluno.ufop.edu.br>
2019-04-04 16:37:42 -0300

    yolo

--- testDisplacement.py
+++ testDisplacement.py
@@ -10,17 +10,18 @@ mpuSensor = mpu6050(0x68)
 
 if __name__ == '__main__':
     try:
-        time1 = []
-        accelerationY = []
-        time1.append(time.time())
-        for i in range(0, 3):
-            accelData = mpuSensor.get_accel_data()
-            accelerationY.append(accelData['y'])
-        time1.append(time.time())
-        deltaT = calc_deltat(time1)
-        velocity_Y = integration_trapeze(0, 3, accelerationY, deltaT)
-        print "Velocity = ", velocity_Y
-        time.sleep(0.5)
+        while True:
+            time1 = []
+            accelerationY = []
+            time1.append(time.time())
+            for i in range(1, 4):
+                accelData = mpuSensor.get_accel_data()
+                accelerationY.append(accelData['y'])
+            time1.append(time.time())
+            deltaT = calc_deltat(time1)
+            velocity_Y = integration_trapeze(1, 4, accelerationY, deltaT)
+            print "Velocity = ", velocity_Y
+            time.sleep(0.5)
     except KeyboardInterrupt:
         print ("Stopping")
         GPIO.cleanup()
